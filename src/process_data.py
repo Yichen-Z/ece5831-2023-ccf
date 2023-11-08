7b1293e9a05e70cfc8d837abde4f56b65e98124e
Yichen Zhang <zhangyichen9@gmail.com>
2023-11-07 23:19:41 -0500

    add functionality to cut transactions dataframe from 2.4 to 2.0 GB

--- src/process_data.py
+++ src/process_data.py
@@ -2,6 +2,19 @@ import joblib
 import pandas as pd
 
 
+def save_space(df: pd.DataFrame) -> pd.DataFrame:
+    df.drop(columns=['Merchant Name'], inplace=True)
+
+    df['MCC'] = df['MCC'].astype('int32')
+
+    df['Amount'] = df['Amount'].apply(lambda value: float(value[1:]))
+    df['Amount'] = df['Amount'].astype('float32')
+
+    df['Use Chip'] = df['Use Chip'].apply(lambda word: word.split()[0])
+
+    return df
+
+
 def get_zero_padded(input_number: int) -> str:
     return f"{input_number:02}"
 
@@ -18,6 +31,7 @@ def combine_date_cols(columns: list, result_col: str, format_str: str, df: pd.Da
 
 if __name__ == '__main__':
     raw_df = pd.read_csv('../data/credit_card_transactions-ibm_v2.csv')
+    raw_df = save_space(df=raw_df)
     set_zero_padded_cols(columns=['Month', 'Day'], df=raw_df)
     combine_date_cols(columns=['Year', 'Month', 'Day'], result_col='transaction_datetime',
                       format_str='%Y-%m-%d')
