ec9cc582d5f786a0367e1ad15fe7f828bb8768ac
Yichen Zhang <zhangyichen9@gmail.com>
2023-11-18 21:06:34 -0500

    combine all data processing into one and split raw transaction dataframe up to see if process can work

--- src/process_data.py
+++ src/process_data.py
@@ -1,39 +1,45 @@
 import joblib
+import numpy as np
+import os
 import pandas as pd
 
 
 def save_space(df: pd.DataFrame) -> pd.DataFrame:
-    df.drop(columns=['Merchant Name'], inplace=True)
-
-    df['MCC'] = df['MCC'].astype('int32')
-
-    df['Amount'] = df['Amount'].apply(lambda value: float(value[1:]))
-    df['Amount'] = df['Amount'].astype('float32')
-
-    df['Use Chip'] = df['Use Chip'].apply(lambda word: word.split()[0])
-
+    df['User'] = df['User'].astype(np.int8)
+    df['Card'] = df['Card'].astype(np.int8)
+    df = get_slimmed_numerics(df)
+    df['Use_Chip'] = df['Use_Chip'].apply(lambda word: word.split()[0])
     return df
 
 
-def get_zero_padded(input_number: int) -> str:
-    return f"{input_number:02}"
+def get_numeric_amount(df: pd.DataFrame) -> None:
+    df['Amount'] = df['Amount'].apply(lambda value: float(value[1:]))
+    df['Amount'] = df['Amount'].astype('float32')
 
 
-def set_zero_padded_cols(columns: list, df: pd.DataFrame) -> None:
-    for col in columns:
-        df[col] = df[col].apply(get_zero_padded)
+def get_slimmed_numerics(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.astype({col: 'int32' for col in df.select_dtypes('int64').columns})
+    df = df.astype({col: 'float32' for col in df.select_dtypes('float64').columns})
+    return df
 
 
-def combine_date_cols(columns: list, result_col: str, format_str: str, df: pd.DataFrame, error_option: str = 'coerce') -> None:
-    df[result_col] = pd.to_datetime(df[columns], format=format_str, errors=error_option)
+def combine_date_cols(columns: list, result_col: str, df: pd.DataFrame, format_str: str = '%Y-%m-%d %H:%M', error_option: str = 'coerce') -> None:
+    df[result_col] = df[columns[0]].astype(str) + '-' + df[columns[1]].astype(str) + '-' + \
+        df[columns[2]].astype(str) + ' ' + df[columns[3]]
+    df[result_col] = pd.to_datetime(df[result_col], format=format_str, cache=True, errors=error_option)
     df.drop(columns=columns, inplace=True)
 
 
 if __name__ == '__main__':
-    raw_df = pd.read_csv('../data/credit_card_transactions-ibm_v2.csv')
-    raw_df = save_space(df=raw_df)
-    set_zero_padded_cols(columns=['Month', 'Day'], df=raw_df)
-    combine_date_cols(columns=['Year', 'Month', 'Day'], result_col='transaction_datetime',
-                      format_str='%Y-%m-%d', df=raw_df)
-    raw_df['Time'] = pd.to_datetime(raw_df['Time'], format='%H:%M', errors='coerce').dt.time
-    joblib.dump(raw_df, '../data/transactions')
+    COL_NAMES = ['User','Card','Year','Month','Day','Time','Amount','Use_Chip',
+                 'Merchant_Name','Merchant_City','Merchant_State','Zip','MCC','Errors','Is_Fraud']
+    PARSE_DATES = ['Year', 'Month', 'Day', 'Time']
+    parts = [f for f in os.listdir('../data/split/') if f.endswith('.csv')]
+    i = 0
+    for part in parts:
+        temp_df = pd.read_csv(part, header=None, names=COL_NAMES)
+        combine_date_cols(columns=PARSE_DATES, result_col='Datetime', df=temp_df)
+        get_numeric_amount(df=temp_df)
+        temp_df = save_space(temp_df)
+        joblib.dump(temp_df, f'../data/slim/transactions_{i}')
+        i += 1
